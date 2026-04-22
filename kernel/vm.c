348f554be1ecc4da556a11cff194c75928faf197
Bao Nguyen <nngbao2413@clc.fitus.edu.vn>
2026-04-22 15:46:03 +0000

    Task 2: vmprint recursive page table printing

--- kernel/vm.c
+++ kernel/vm.c
@@ -449,3 +449,33 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+void
+vmprint_helper(pagetable_t pagetable, int level)
+{
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+
+    if(pte & PTE_V){
+      uint64 pa = PTE2PA(pte);
+
+      // indent
+      for(int j = 0; j < level; j++){
+        printf(" ..");
+      }
+
+      printf("%d: pte %p pa %p\n", i, (void*)pte, (void*)pa);
+
+      // if NOT leaf → go deeper
+      if((pte & (PTE_R | PTE_W | PTE_X)) == 0){
+        vmprint_helper((pagetable_t)pa, level + 1);
+      }
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  vmprint_helper(pagetable, 1);
+}
